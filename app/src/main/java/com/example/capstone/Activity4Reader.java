c40ade5d84890c274e00d579fbd2c1da4d8c52e8
kcysh <0927jennifer@gmail.com>
2022-04-14 07:21:02 +0900

    댓글 추가

--- app/src/main/java/com/example/capstone/Activity4Reader.java
+++ app/src/main/java/com/example/capstone/Activity4Reader.java
@@ -1,14 +1,44 @@
 package com.example.capstone;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class Activity4Reader extends AppCompatActivity {
+    private ArrayList<item2> iList2;
+    private RecyclerView commentRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity4_reader);
+
+        commentRecyclerView = findViewById(R.id.commentRecyclerView);
+        iList2 = new ArrayList<>();
+
+        setItemInfo();
+        setAdapter();
+    }
+
+    //리사이클러뷰
+    private void setAdapter() {
+        commentRecyclerAdapter adapter = new commentRecyclerAdapter(iList2);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        commentRecyclerView.setLayoutManager(layoutManager);
+        commentRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        commentRecyclerView.setAdapter(adapter);
+    }
+    private void setItemInfo() {
+        iList2.add(new item2("익명1","댓글예시1 입니다."));
+        iList2.add(new item2("익명2","댓글예시2 입니다."));
+        iList2.add(new item2("익명3","댓글예시3 입니다."));
+        iList2.add(new item2("익명4","댓글예시4 입니다."));
+        iList2.add(new item2("익명5","댓글예시5 입니다."));
+
     }
 }
