1e888525c2374677eaa370a6a0f1edd8fe15583a
user <wlsgur831@naver.com>
2022-04-19 11:16:54 +0900

    코틀린 파일 지움

--- app/src/main/java/com/example/capstone/ResultDTO.kt
+++
@@ -1,8 +0,0 @@
-package com.example.capstone
-
-data class ResultDTO (
-        var uid :String? = null,
-        var textOne : String? = null,
-        var textTwo : String? = null,
-        var timestamp : Long? = null
-)
