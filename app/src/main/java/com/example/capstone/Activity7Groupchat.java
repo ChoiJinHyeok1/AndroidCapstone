121958a770cb573255ad701671ae93278769517e
user <rlacksgur991110@gmail.com>
2022-04-19 12:12:11 +0900

    찬혁

--- app/src/main/java/com/example/capstone/Activity7Groupchat.java
+++ app/src/main/java/com/example/capstone/Activity7Groupchat.java
@@ -3,12 +3,24 @@ package com.example.capstone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Activity7Groupchat extends AppCompatActivity {
+    Button btnOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity7_groupchat);
+
+        btnOut = findViewById(R.id.btn_Out);
+
+        btnOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
