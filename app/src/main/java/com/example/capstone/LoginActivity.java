d2379a2369a651049335f956545468320966664c
user <wlsgur831@naver.com>
2022-05-03 10:58:32 +0900

    로그인 회원가입 추가

--- app/src/main/java/com/example/capstone/LoginActivity.java
+++ app/src/main/java/com/example/capstone/LoginActivity.java
@@ -43,7 +43,7 @@ public class LoginActivity extends AppCompatActivity {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if (task.isSuccessful()) {//성공했을때
-                                    Intent intent = new Intent(LoginActivity.this, Activity3Main.class);
+                                    Intent intent = new Intent(LoginActivity.this, Activity5Main.class);
                                     startActivity(intent);
                                 } else {//실패했을때
                                     Toast.makeText(LoginActivity.this, "로그인 오류", Toast.LENGTH_SHORT).show();
