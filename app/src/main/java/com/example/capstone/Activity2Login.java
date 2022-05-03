01a4c9a8e31dfd713b3f56d2e950b40abfcc29ee
user <wlsgur831@naver.com>
2022-05-03 11:07:12 +0900

    로그인 회원가입

--- app/src/main/java/com/example/capstone/Activity2Login.java
+++ app/src/main/java/com/example/capstone/Activity2Login.java
@@ -1,14 +1,65 @@
 package com.example.capstone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Activity2Login extends AppCompatActivity {
-
+    private Button join;
+    private Button login;
+    private EditText email_login;
+    private EditText pwd_login;
+    FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity2_login);
+        join = (Button) findViewById(R.id.join);
+        login = (Button) findViewById(R.id.login);
+        email_login = (EditText) findViewById(R.id.email_login);
+        pwd_login = (EditText) findViewById(R.id.pwd_login);
+        firebaseAuth = firebaseAuth.getInstance();//firebaseAuth의 인스턴스를 가져옴
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = email_login.getText().toString().trim();
+                String pwd = pwd_login.getText().toString().trim();
+                //String형 변수 email.pwd(edittext에서 받오는 값)으로 로그인하는것
+                firebaseAuth.signInWithEmailAndPassword(email, pwd)
+                        .addOnCompleteListener(Activity2Login.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {//성공했을때
+                                    Intent intent = new Intent(Activity2Login.this, Activity4Certified.class);
+                                    startActivity(intent);
+                                } else {//실패했을때
+                                    Toast.makeText(Activity2Login.this, "로그인 오류", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+            }
+        });
+
+        join.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Activity2Login.this, Activity3SignUp.class);
+                startActivity(intent);
+            }
+        });
     }
 }
